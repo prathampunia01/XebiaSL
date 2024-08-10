00c0b7fa2033acbd49228e0c9c00858c7643084f
Pratham Punia <159421331+prathampunia01@users.noreply.github.com>
2024-08-10 20:30:52 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -1,3 +1,4 @@
+#Write a function that takes an integer and returns the sum of its digits. For example, for the number 1234, the output should be 10 (1 + 2 + 3 + 4).
 def sumDigits(n):
     n = abs(n)  
     sumDigits = 0

302ac0d1a9bdb0d8eae2bf518efcbc53c5dc9955
Pratham Punia <159421331+prathampunia01@users.noreply.github.com>
2024-08-10 20:31:27 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -1,3 +1,4 @@
+#Write a function to check if a given number is prime using a loop. For example, is_prime(29) should return True, and is_prime(10) should return False.
 def isprime(n):
     if n <= 1:
         return False 
